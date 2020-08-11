850729b4df5fcf7fa9353d4428b064928f912d68
cprice <cprice@tutanota.com>
2020-08-11 19:31:35 +0200

    Rethrow to not encapsulate error

--- lib/index.ts
+++ lib/index.ts
@@ -12,12 +12,12 @@ export const throwError = (name: string, msg: string, extraInfo?: any): never =>
   throw new FError(name, msg, extraInfo)
 }
 
-export const reThrow = (name: string) => (e: Error) => throwError(name, e.message, e)
+export const reThrow = (e: Error) => { throw e }
 
 export const on = (pattern: string, fn: Function) => (event: FError) =>
   event.name === pattern
     ? fn(event.extraInfo)
-    : reThrow(event.name)(event)
+    : reThrow(event)
 
 export default {
   throwError,
